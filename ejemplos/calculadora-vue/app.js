e16441b0620a0d03294ff8121a1f279dc5adc1ae
Alex Casadevall <alex@lexartlabs.com>
2022-04-28 20:25:16 -0300

    app vue example

---
+++ ejemplos/calculadora-vue/app.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!',
+      nombre: "Alex",
+      persona: {
+          nombre: "Juan",
+          edad: 28
+      }
+    }
+})
